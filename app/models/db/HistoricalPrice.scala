e24e8f821127c60b8e81204c872d96e0a0f7b70a
Benjamin Moore <american.exceptionalist@gmail.com>
2017-11-29 20:29:43 -0500

    Better error handling pattern

--- app/models/db/HistoricalPrice.scala
+++ app/models/db/HistoricalPrice.scala
@@ -5,9 +5,10 @@ import javax.inject.{Inject, Singleton}
 
 import play.api.Logger
 import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 final case class HistoricalPrice(coinSlug: String, price_date: Timestamp, price: Double, priceUnits: String) {}
 
@@ -23,16 +24,22 @@ final class HistoricalPricesTable(tag: Tag) extends Table[HistoricalPrice](tag, 
 }
 
 @Singleton
-class HistoricalPriceDAO @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) {
+class HistoricalPriceDAO @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
 
   val prices = TableQuery[HistoricalPricesTable]
   val db = dbConfigProvider.get.db
 
   def findAll(): Future[Seq[HistoricalPrice]] = {
-    db.run(prices.result)
+    db.run(prices.result).recover {
+      case exception: Throwable => Logger.error("findAll failed", exception)
+        return Future.failed(exception)
+    }
   }
 
-  def insert(newPrices: HistoricalPrice*): Unit = {
-    db.run(prices ++= newPrices).recover { case exception: Throwable => Logger.error("Insert failed", exception) }
+  def insert(newPrices: HistoricalPrice*): Future[PostgresProfile.InsertActionExtensionMethods[HistoricalPricesTable#TableElementType]#MultiInsertResult] = {
+    db.run(prices ++= newPrices).recover {
+      case exception: Throwable => Logger.error("insert failed", exception)
+        return Future.failed(exception)
+    }
   }
 }
--- app/tasks/FetchPriceTask.scala
+++ app/tasks/FetchPriceTask.scala
@@ -11,11 +11,42 @@ import play.api.libs.ws.WSClient
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 
-case class Ticker(price_usd: String, last_updated: String) {}
+case class Ticker(price_usd: String, last_updated: String)
+class FailedHTTPRequest(message: String) extends Exception
+class FailedJSONParse(message: String) extends Exception
 
 class FetchPriceTask @Inject()(actorSystem: ActorSystem, ws: WSClient, coinsDao: CoinDAO, pricesDAO: HistoricalPriceDAO)(implicit executionContext: ExecutionContext) {
+
+  /*
+  We fetch all coin types, which returns a future, then unwrap that future's result list and schedule a job for each
+  item in it.
+
+  Each job:
+      Makes web request
+        |- Success
+        |      |- Transforms json to Ticker
+        |          |- Success
+        |          |   |- Transforms Ticker to Future[MultiInsertResult]
+        |          |- Failure
+        |              |- Logs Exception
+        |- Failure
+            |- Logs Exception
+   */
+
+  coinsDao.findAll().map(coins => {
+    coins.foreach(coin => {
+      actorSystem.scheduler.schedule(0.seconds, 15.minutes) {
+        getPriceJson(transformToAPI(coin.name)).map(jsOpt =>
+          jsOpt.map(js =>
+            jsonToTicker(js).map(ticker =>
+              pricesDAO.insert(HistoricalPrice(coin.slug, new Timestamp(ticker.last_updated.toLong*1000), ticker.price_usd.toDouble, "USD"))))
+        )
+      }
+    })
+  })
 
   implicit val tickerReads: Reads[Ticker] = Json.reads[Ticker]
 
@@ -23,57 +54,41 @@ class FetchPriceTask @Inject()(actorSystem: ActorSystem, ws: WSClient, coinsDao:
     coinName.toLowerCase.replace(" ", "-")
   }
 
-  def getPriceJson(coin: String): Future[Option[JsValue]] = {
+  def getPriceJson(coin: String): Future[Try[JsValue]] = {
     val response = ws.url("https://api.coinmarketcap.com/v1/ticker/" + coin).get()
     response.map(resp => if(resp.status == 200) {
-      Some(resp.json)
+      Success(resp.json)
     } else {
-      Logger.error("Error: " + resp.status + ": " + resp.statusText)
-      None
+      val message = "Error: " + resp.status + ": " + resp.statusText
+      val failure = new FailedHTTPRequest(message)
+      Logger.error(message, failure)
+      Failure(failure)
     })
   }
 
-  def jsonToTicker(jsValue: JsValue): Option[Ticker] = {
+  def jsonToTicker(jsValue: JsValue): Try[Ticker] = {
     jsValue match {
       case JsArray(values) =>
         val result = Json.fromJson[Ticker](values(0))
 
         result.fold(errors => {
+          var errorMsg = ""
           errors.foreach(tuple => {
-            var errorMsg = "Error while parsing: " + tuple._1.toString() + "\nFailed Paths: "
-            tuple._2.foreach(error => errorMsg += error.message)
-            Logger.error(errorMsg)
+            errorMsg += "Error while parsing: " + tuple._1.toString() + "\nFailed Paths: "
+            tuple._2.foreach(error => errorMsg += "\t" + error.message)
           })
-          None
-        }, ticker => Some(ticker))
+          val failure = new FailedJSONParse(errorMsg)
+          Logger.error(errorMsg, failure)
+          Failure(failure)
+        }, {
+          ticker => Success(ticker)
+        })
 
       case _ =>
-        Logger.error("Error: unknown json response format")
-        None
+        val message = "Error: unknown json response format"
+        val failure = new FailedJSONParse(message)
+        Logger.error(message, failure)
+        Failure(failure)
     }
   }
-
-  coinsDao.findAll().map(coins => {
-
-    coins.foreach(coin => {
-      actorSystem.scheduler.schedule(0.seconds, 15.minutes) {
-
-        getPriceJson(transformToAPI(coin.name)).map(jsOpt => {
-          val resultTree = jsOpt.map(js =>
-            jsonToTicker(js).map(ticker =>
-              HistoricalPrice(coin.slug, new Timestamp(ticker.last_updated.toLong*1000), ticker.price_usd.toDouble, "USD")))
-
-          // resultTree encapsulates the possibility that the original fetching of the price json failed, as well as
-          // the possibility that converting the received json to a Ticker failed. Strip off the opts and store the
-          // price, if present
-          for {
-            priceOpt <- resultTree
-            b <- priceOpt
-          } {
-            pricesDAO.insert(b)
-          }
-        })
-      }
-    })
-  })
 }
