5f135f0ffb2afdab5a60a7b71ac4facfd92713b1
Benjamin Moore <american.exceptionalist@gmail.com>
2017-11-30 16:41:50 -0500

    Addition of UI scaffolding

--- app/controllers/PurchaseController.scala
+++ app/controllers/PurchaseController.scala
@@ -4,7 +4,7 @@ import javax.inject.{Inject, Singleton}
 
 import models.db.{Purchase, PurchaseDAO}
 import play.api.libs.json.Json
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -12,7 +12,7 @@ import scala.util.{Failure, Success}
 @Singleton
 class PurchaseController @Inject()(cc: ControllerComponents, purchaseDAO: PurchaseDAO)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def create = Action.async { implicit request: Request[AnyContent] =>
+  def create: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
     val json = request.body.asJson.get
     val result = Json.fromJson[Purchase](json)
 
